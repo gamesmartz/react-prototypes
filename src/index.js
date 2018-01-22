a480742dc30ac0b7a848b426298fe3c3f302299e
gamesmartz <awnold@hotmail.com>
2018-01-22 13:34:43 -0800

    proto done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function luckyNumber () {
+    return Math.floor(Math.random() * (1000 - 0) + 1);
+}
+
+var luckyNumberStorage = {
+    name: 'Matt',
+    number: luckyNumber(),
+};
+
+function greeting (user) {
+    return (
+        <div className="container">
+            <h1>Hello {user.name}</h1>
+            <h2 className="text-muted">Your Lucky Number is: {user.number}</h2>
+        </div>
+    )
+}
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(luckyNumberStorage),
     document.getElementById('root')
+
 );
