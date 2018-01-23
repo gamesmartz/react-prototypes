aeb62d243a9f22c23f030285de9cecf71b0f91fb
gamesmartz <awnold@hotmail.com>
2018-01-23 15:00:58 -0800

    proto done

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import StudentTable from './table';
+
+const students = [
+    {
+        student: 'Bruce Wayne',
+        course: 'Criminal Justice',
+        grade: '94'
+    },
+    {
+        student: 'Barry Allen',
+        course: 'Forensic Science',
+        grade: '98'
+    },
+    {
+        student: 'Clark Kent',
+        course: 'Creative Writing',
+        grade: '86'
+    }
+];
+
 
 function App() {
-    return <h1>My first React functional component</h1>
+    return <div className="container">
+        <h1>Student Grade Table</h1>
+
+        <StudentTable data={students} />
+    </div>
 }
 
+export default App;
---
+++ src/components/table.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function StudentTable (props){
+
+    const tableRows = props.data.map((item, index) => {
+        return <tr key={index}>
+                    <td>{item.student}</td>
+                    <td>{item.course}</td>
+                    <td>{item.grade}</td>
+                </tr>
+    });
+
+    return (
+        <table className="table">
+            <thead className="thead-inverse">
+                <tr>
+                    <th>Name</th>
+                    <th>Course</th>
+                    <th>Grade</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            {tableRows}
+
+            </tbody>
+        </table>
+    )
+
+}
+
+export default StudentTable;
